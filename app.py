9dc65f2505294d94d737906c9d8f185cfab6140e
anggraeniervana098 <129573809+anggraeniervana098@users.noreply.github.com>
2023-11-30 10:27:45 +0000

    update app

--- app.py
+++ app.py
@@ -1,71 +1,74 @@
 import streamlit as st
 from sqlalchemy import text
 
-list_doctor = ['', 'dr. Nurita', 'dr. Yogi', 'dr. Wibowo', 'dr. Ulama', 'dr. Ping']
-list_symptom = ['', 'male', 'female']
+list_symptom = ['', 'Pria', 'Wanita']
+list_posisi = ['','Penyerang', 'Gelandang', 'Bertahan', 'Kiper']
 
 conn = st.connection("postgresql", type="sql", 
                      url="postgresql://anggraeniervana:yMcg1tW6CRaL@ep-late-field-02299309.us-east-2.aws.neon.tech/web")
 with conn.session as session:
-    query = text('CREATE TABLE IF NOT EXISTS SCHEDULE (id serial, doctor_name varchar, patient_name varchar, gender char(25), \
-                                                       symptom text, handphone varchar, address text, tanggal date);')
+    query = text('CREATE TABLE IF NOT EXISTS BIODATA (id serial, nama_pemain varchar, nama_pelatih varchar, jenis_kelamin char(25), \
+                                                       posisi text, asal_club varchar, asal_negara varchar, tempat_lahir varchar, tanggal_lahir date, tinggi_badan varchar);')
     session.execute(query)
 
-st.header('SIMPLE HOSPITAL DATA MANAGEMENT SYSTEM')
+st.header('BIODATA PEMAIN BOLA TERBAIK')
 page = st.sidebar.selectbox("Pilih Menu", ["View Data","Edit Data"])
 
 if page == "View Data":
-    data = conn.query('SELECT * FROM schedule ORDER By id;', ttl="0").set_index('id')
+    data = conn.query('SELECT * FROM biodata ORDER By id;', ttl="0").set_index('id')
     st.dataframe(data)
 
 if page == "Edit Data":
     if st.button('Tambah Data'):
         with conn.session as session:
-            query = text('INSERT INTO schedule (doctor_name, patient_name, gender, symptom, handphone, address, waktu, tanggal) \
-                          VALUES (:1, :2, :3, :4, :5, :6, :7, :8);')
-            session.execute(query, {'1':'', '2':'', '3':'', '4':'[]', '5':'', '6':'', '7':None, '8':None})
+            query = text('INSERT INTO biodata (nama_pemain, nama_pelatih, jenis_kelamin, \
+                                                       posisi, asal_club, asal_negara, tempat_lahir, tanggal_lahir, tinggi_badan) \
+                          VALUES (:1, :2, :3, :4, :5, :6, :7, :8, :9);')
+            session.execute(query, {'1':'', '2':'', '3':'', '4':'[]', '5':'', '6':'', '7':'', '8':None, '9':''})
             session.commit()
 
-    data = conn.query('SELECT * FROM schedule ORDER By id;', ttl="0")
+    data = conn.query('SELECT * FROM biodata ORDER By id;', ttl="0")
     for _, result in data.iterrows():        
         id = result['id']
-        doctor_name_lama = result["doctor_name"]
-        patient_name_lama = result["patient_name"]
-        gender_lama = result["gender"]
-        symptom_lama = result["symptom"]
-        handphone_lama = result["handphone"]
-        address_lama = result["address"]
-        waktu_lama = result["waktu"]
-        tanggal_lama = result["tanggal"]
+        nama_pemain_lama = result["nama_pemain"]
+        nama_pelatih_lama = result["nama_pelatih"]
+        jenis_kelamin_lama = result["jenis_kelamin"]
+        posisi_lama = result["posisi"]
+        asal_club_lama = result["asal_club"]
+        asal_negara_lama = result["asal_negara"]
+        tempat_lahir_lama = result["tempat_lahir"]
+        tanggal_lahir_lama = result["tanggal_lahir"]
+        tinggi_badan_lama = result["tinggi_badan"]
 
-        with st.expander(f'a.n. {patient_name_lama}'):
+        with st.expander(f'a.n. {nama_pemain_lama}'):
             with st.form(f'data-{id}'):
-                doctor_name_baru = st.selectbox("doctor_name", list_doctor, list_doctor.index(doctor_name_lama))
-                patient_name_baru = st.text_input("patient_name", patient_name_lama)
-                gender_baru = st.selectbox("gender", list_symptom, list_symptom.index(gender_lama))
-                symptom_baru = st.multiselect("symptom", ['cough', 'flu', 'headache', 'stomache'], eval(symptom_lama))
-                handphone_baru = st.text_input("handphone", handphone_lama)
-                address_baru = st.text_input("address", address_lama)
-                waktu_baru = st.time_input("waktu", waktu_lama)
-                tanggal_baru = st.date_input("tanggal", tanggal_lama)
+                nama_pemain_baru = st.selectbox("nama_pemain", nama_pemain_lama)
+                nama_pelatih_baru = st.text_input("nama_pelatih", nama_pelatih_lama)
+                jenis_kelamin_baru = st.selectbox("jenis_kelamin", list_symptom, list_symptom.index(jenis_kelamin_lama))
+                posisi_baru = st.multiselect("posisi", ['cough', 'flu', 'headache', 'stomache'], eval(posisi_lama))
+                asal_club_baru = st.text_input("asal_club", asal_club_lama)
+                asal_negara_baru = st.text_input("asal_negara", asal_negara_lama)
+                tempat_lahir_baru = st.text_input("tempat_lahir", tempat_lahir_lama)
+                tanggal_lahir_baru = st.date_input("tanggal_lahir", tanggal_lahir_lama)
+                tinggi_badan_baru = st.text_input("tinggi_badan", tinggi_badan_lama)
                 
                 col1, col2 = st.columns([1, 6])
 
                 with col1:
                     if st.form_submit_button('UPDATE'):
                         with conn.session as session:
-                            query = text('UPDATE schedule \
-                                          SET doctor_name=:1, patient_name=:2, gender=:3, symptom=:4, \
-                                          handphone=:5, address=:6, waktu=:7, tanggal=:8 \
-                                          WHERE id=:9;')
-                            session.execute(query, {'1':doctor_name_baru, '2':patient_name_baru, '3':gender_baru, '4':str(symptom_baru), 
-                                                    '5':handphone_baru, '6':address_baru, '7':waktu_baru, '8':tanggal_baru, '9':id})
+                            query = text('UPDATE biodata \
+                                          SET nama_pemain=:1, nama_pelatih=:2, jenis_kelamin=:3, posisi=:4, \
+                                          asal_club=:5, asal_negara=:6, tempat_lahir=:7, tanggal_lahir=:8, tinggi_badan=9 \
+                                          WHERE id=:10;')
+                            session.execute(query, {'1':nama_pemain_baru, '2':nama_pelatih_baru, '3':jenis_kelamin_baru, '4':str(posisi_baru), 
+                                                    '5':asal_club_baru, '6':asal_negara_baru, '7':tempat_lahir_baru, '8':tanggal_lahir_baru, '9':tinggi_badan_baru, '10':id})
                             session.commit()
                             st.experimental_rerun()
                 
                 with col2:
                     if st.form_submit_button('DELETE'):
-                        query = text(f'DELETE FROM schedule WHERE id=:1;')
+                        query = text(f'DELETE FROM biodata WHERE id=:1;')
                         session.execute(query, {'1':id})
                         session.commit()
                         st.experimental_rerun()
